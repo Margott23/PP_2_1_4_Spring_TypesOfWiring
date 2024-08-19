76427b1c8e657799d23b3a2ca82df186dfedf2ec
Margott23 <margott23@mail.ru>
2024-08-19 07:56:35 +0300

    done

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "\nВот и смерть Кощеева пришла!";
+        // Решил добавить конец сказки, для красоты. Конечно же, можно просто "" вернуть.
+    }
 }
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
+    private Rabbit4 rabbit;
 
     @Autowired
-    public Wood3(Rabbit4 rabbit) {
+    public void setRabbit(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
 
     @Override
     public String toString() {
-        return ", на дереве заяц " + rabbit.toString();
+        return ", на дереве заяц" + rabbit.toString();
     }
 }
